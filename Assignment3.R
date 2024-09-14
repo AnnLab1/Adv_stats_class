97a4ba1591d8f24299c6dd5fb405292aca580345
AnnLab1 <150186647+AnnLab1@users.noreply.github.com>
2024-09-14 08:49:47 -0400

    Add files via upload

---
+++ Assignment3.R
@@ -0,0 +1,23 @@
+set1 <- c(10,2,3,2,4,2,5)
+set2 <- c(20,12,13,12,14,12,15)
+ 
+summary(set1)
+range(set1)
+iqv = 4.5-2
+iqv
+var(set1)
+sd(set1)
+#modeSet1 = 2
+
+summary(set2)
+range(set2)
+iqv = 14.5-12
+iqv
+var(set2)
+sd(set2)
+#modeSet2 = 12
+
+#Set1 has a lower minimun, mean, median, and max when compared to set2
+#Set2 and set1 has the same interquartile variance 
+#Both datasets share the same range, variance, and standard deviation
+ 
